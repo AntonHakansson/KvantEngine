c1b5dea2189f71e5f88196ebc4b52f3402030b30
AntonHakansson <anton.hakansson98@gmail.com>
2017-12-26 17:58:23 +0100

    Added scene-manager scene

--- examples/basic_rendering.cpp
+++ examples/basic_rendering.cpp
@@ -2,11 +2,13 @@
 
 #include "backends/sdl2/sdl2.hpp"
 #include "backends/opengl/Opengl.hpp"
+#include "core/Loader.hpp"
 
 using my_app = Kvant::App<Kvant::platform::sdl::BackendForOpengl,
                                   Kvant::graphics::opengl::Backend,
                                   Kvant::Context>;
 class SpaceScene;
+class CrateScene;
 class IntroScene : public my_app::Scene {
     public:
         IntroScene(const my_app::Context &ctx)
@@ -34,8 +36,8 @@ class IntroScene : public my_app::Scene {
         }
             
         void update_scenes(const my_app::Context& ctx) override {
-            if (_change_to_space)
-                ctx.scenes->push_scene<SpaceScene>(ctx);
+            if (_change_to_space) ctx.scenes->push_scene<SpaceScene>(ctx);
+            if (_change_to_crate) ctx.scenes->push_scene<CrateScene>(ctx);
         }
         
         void resume(const my_app::Context& ctx) override { LOG_DEBUG << "MENu: Im Resumed!!"; }
@@ -44,29 +46,29 @@ class IntroScene : public my_app::Scene {
         void imgui(const my_app::Context &ctx) override {
             _camera.entity_ptr()->imgui();
             _change_to_space = false;
-            if (ImGui::CollapsingHeader("Scenes")) {
-                if (ImGui::Button("NEXT SCENE")) {
-                    _change_to_space = true;
-                }
+            if (ImGui::CollapsingHeader("Example Scenes")) {
+                _change_to_space = ImGui::Button("MOON");
+                _change_to_crate = ImGui::Button("CRATE");
             }
-
         }
 
     private:
         Kvant::Instance<Kvant::Camera> _camera;
         my_app::ForwardPipeline _pipeline;
         std::function<void()> _quit;
-        bool _change_to_space{false};
+        bool _change_to_space{false}, _change_to_crate{false};
 };
     
+Kvant::RawModelData _model_data(Kvant::read_model("res/examples/moon.fbx"));
+std::string resDir = "res/examples/";
 class Moon : public my_app::Asset<Moon>,
              public my_app::Drawable<Moon> {
 
     public:
-        Moon(const my_app::Context& ctx) : my_app::Drawable<Moon>(ctx, "res/examples/moon.smd"),
-                                           _diffuse_map(ctx, "res/examples/moon_d.png"),
-                                           _normal_map(ctx, "res/examples/moon_nm.png"),
-                                           _specular_map(ctx, "res/examples/moon_s.png"),
+        Moon(const my_app::Context& ctx) : my_app::Drawable<Moon>(ctx, _model_data),
+                                           _diffuse_map(ctx, resDir + _model_data.diffuses[0]),
+                                           _normal_map(ctx, resDir + _model_data.normals[0]),
+                                           _specular_map(ctx, resDir + _model_data.speculars[0]),
                                            _material(ctx, &_diffuse_map, &_normal_map, &_specular_map) {
         }
 
@@ -106,13 +108,13 @@ class SpaceScene : public my_app::Scene {
         }
             
         void update_scenes(const my_app::Context& ctx) override {
-            if (ctx.get_platform()->is_key_pressed(Kvant::key::Q)) {
+            if (_pop_scene) {
                 ctx.scenes->pop_scene(ctx);
             }
         }
 
         void resume(const my_app::Context& ctx) override {
-            LOG_DEBUG << "SPACE: Im paused!!";
+            LOG_DEBUG << "SPACE: Im resumed!!";
         }
         void pause(const my_app::Context& ctx) override {
             LOG_DEBUG << "SPACE: Im paused!!";
@@ -120,6 +122,7 @@ class SpaceScene : public my_app::Scene {
 
         void imgui(const my_app::Context &ctx) override {
             _camera.entity_ptr()->imgui();
+            _pop_scene = ImGui::Button("Return to scene select");
         }
 
     private:
@@ -127,8 +130,84 @@ class SpaceScene : public my_app::Scene {
         Kvant::Driven<Kvant::Instance<Kvant::Camera>, Kvant::HoverDriver> _camera;
         Kvant::wsad_controller  _wasd;
         my_app::ForwardPipeline _pipeline;
+
+        bool _pop_scene{false};
 };
 
+class Crate : public my_app::Asset<Crate>,
+              public my_app::Drawable<Crate>
+{
+  private:
+    my_app::Texture _diffuse_map, _normal_map, _specular_map;
+    my_app::Material _material;
+
+  public:
+    Crate(const my_app::Context &ctx)
+        : my_app::Drawable<Crate>{ctx, "res/examples/crate2.smd"},
+          _diffuse_map{ctx, "res/examples/crate2_d.png"},
+          _normal_map{ctx, "res/examples/crate2_n.png"},
+          _specular_map{ctx, "res/examples/crate2_s.png"},
+          _material{ctx, &_diffuse_map, &_normal_map, &_specular_map}
+    {
+    }
+    const my_app::Material &get_material() const
+    {
+        return _material;
+    }
+};
+
+class CrateScene : public my_app::Scene {
+    public:
+        CrateScene(const my_app::Context& ctx) 
+            : _camera{ctx, Kvant::PovDriver::look_at({0.0, 0.0, -50.0}, {0.0, 0.0, 0.0}), &ctx.p->screen()},
+              _pipeline(ctx),
+              _crate(ctx, Kvant::pos::origin) {
+        }
+              
+        void update(const my_app::Context& ctx) override {
+            _wasd.update(ctx, _camera.get_driver_ptr());
+
+            render(ctx);
+        }
+
+        void render(const my_app::Context& ctx) override {
+            my_app::RenderPass(ctx)
+                .target(my_app::Graphics::screen_buffer)
+                .clear();
+            
+            _pipeline.use(ctx)
+                .set_camera(*_camera.drivable_ptr())
+                .set_material(_crate.get_drawable().get_material())
+                .draw(_crate);
+        }
+
+        void update_scenes(const my_app::Context& ctx) override {
+            if (_pop_scene) {
+                ctx.scenes->pop_scene(ctx);
+            }
+        }
+
+        void resume(const my_app::Context& ctx) override {
+            LOG_DEBUG << "Crate: Im Resumed!!";
+        }
+        void pause(const my_app::Context& ctx) override {
+            LOG_DEBUG << "Crate: Im paused!!";
+        }
+
+        void imgui(const my_app::Context &ctx) override {
+            _camera.entity_ptr()->imgui();
+            _pop_scene = ImGui::Button("Return to scene select");
+        }
+
+    private:
+        my_app::ForwardPipeline _pipeline;
+        Kvant::Driven<Kvant::Instance<Kvant::Camera>, Kvant::HoverDriver> _camera;
+        Kvant::wsad_controller _wasd;
+
+        Kvant::Instance<Crate> _crate;
+        bool _pop_scene{false};
+ };
+    
 int main() {
     try {
         my_app().run<IntroScene>();
